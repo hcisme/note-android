4d86a2169ea3d5f8751f4fa1604d05dd76d3ce2a
chihaicheng <chc2075983104@163.com>
2025-10-25 07:28:48 +0800

    feat: 增加相关版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "io.github.hcisme.note"
         minSdk = 24
         targetSdk = 36
-        versionCode = (System.getenv("VERSION_CODE") ?: "1").toInt()
-        versionName = System.getenv("VERSION_NAME") ?: "1.0"
+        versionCode = (System.getenv("VERSION_CODE") ?: "23").toInt()
+        versionName = System.getenv("VERSION_NAME") ?: "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
